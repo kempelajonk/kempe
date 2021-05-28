0faeda1ec1d5f90358563874a4d07028c0dd1b9a
kempelajonk <84513484+kempelajonk@users.noreply.github.com>
2021-05-28 19:15:10 +0800

    Update bikinbaru.js

--- bikinbaru.js
+++ bikinbaru.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
-  'https://dartoverboardcoupon.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15',
-  'https://dartoverboardcoupon.com/wz9gam9e?key=29baf6f1c6b00231eac45186ca8e462c'
+  'https://chancesmusicalinterrogation.com/wz9gam9e?key=29baf6f1c6b00231eac45186ca8e462c',
+  'https://chancesmusicalinterrogation.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
