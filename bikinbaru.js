dadcae15c7cc3199bb1fa3c93b00c8ac3643ad04
kempelajonk <84513484+kempelajonk@users.noreply.github.com>
2021-05-30 10:57:24 +0800

    Update bikinbaru.js

--- bikinbaru.js
+++ bikinbaru.js
@@ -1,6 +1,5 @@
 var dataUrl = [
   
-  'https://chancesmusicalinterrogation.com/wz9gam9e?key=29baf6f1c6b00231eac45186ca8e462c',
   'https://chancesmusicalinterrogation.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
