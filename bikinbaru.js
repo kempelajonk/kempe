9d3efe4547370a6350908a7bb721bb03010de06b
kempelajonk <84513484+kempelajonk@users.noreply.github.com>
2021-10-09 10:11:33 +0800

    Update bikinbaru.js

--- bikinbaru.js
+++ bikinbaru.js
@@ -3,8 +3,7 @@ var dataUrl = [
   'https://routemomentarilydiscovering.com/wz9gam9e?key=29baf6f1c6b00231eac45186ca8e462c',
   'https://routemomentarilydiscovering.com/a8cwjakuu?key=079bebe14074c0345b33aa3d9edf4c15',
   'https://routemomentarilydiscovering.com/cazjeq0d?key=6ad85b09d0587931bfbcae4e96f6a41e',
-  'https://routemomentarilydiscovering.com/vj0bpt5kb4?key=721b77d4cb4d9e5fbae870ef581bc5cd',
-  'https://routemomentarilydiscovering.com/rtphmzpndu?key=eee2f275fbfc52029bf6d2ef8937105c'
+  'https://routemomentarilydiscovering.com/vj0bpt5kb4?key=721b77d4cb4d9e5fbae870ef581bc5cd'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
